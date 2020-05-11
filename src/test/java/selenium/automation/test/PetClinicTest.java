a7cbb3499623e91c29c3d78085c761dfe819eda2
foreverankit <foreverankit143@gmail.com>
2020-05-11 16:50:52 +0530

    first commit

---
+++ src/test/java/selenium/automation/test/PetClinicTest.java
@@ -0,0 +1,116 @@
+package selenium.automation.test;
+
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.*;
+//import static org.testng.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+//import org.openqa.selenium.phantomjs.PhantomJSDriver;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class PetClinicTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  public static String chromeDriverPath = "/usr/bin/chromedriver";
+  //WebDriver driver = new ChromeDriver();
+  @BeforeClass
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+    options.setBinary("/usr/bin/google-chrome");
+    driver = new ChromeDriver(options);
+    //System.setProperty("webdriver.chrome.driver",chromeDriverPath);
+//	DesiredCapabilities capabilities = DesiredCapabilities.phantomjs();
+//    driver = new PhantomJSDriver(capabilities);
+//    baseUrl = "http://ec2-3-232-123-215.compute-1.amazonaws.com:31090";
+    baseUrl = "http://ecs-petclinic.kaiburr.com:8888/";
+    //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testNewOwnerForm() throws Exception {
+    if (baseUrl == null || baseUrl.isEmpty()) {
+      fail("Empty base URL");
+    }
+    System.out.println("Base URL: " + baseUrl);
+    
+    driver.get(baseUrl);    
+    driver.manage().window().setSize(new Dimension(1366, 720));
+    driver.findElement(By.cssSelector("li:nth-child(3) span:nth-child(2)")).click(); // Find owners
+    driver.findElement(By.linkText("Add Owner")).click();
+    driver.findElement(By.id("firstName")).clear();
+    driver.findElement(By.id("firstName")).sendKeys("siva");
+    driver.findElement(By.id("lastName")).clear();
+    driver.findElement(By.id("lastName")).sendKeys("chedde");
+    driver.findElement(By.id("address")).clear();
+    driver.findElement(By.id("address")).sendKeys("electronic city");
+    driver.findElement(By.id("city")).clear();
+    driver.findElement(By.id("city")).sendKeys("bangalore");
+    driver.findElement(By.id("telephone")).clear();
+    driver.findElement(By.id("telephone")).sendKeys("99999");
+    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+    driver.findElement(By.cssSelector("li:nth-child(3) span:nth-child(2)")).click(); // Find owners
+//    driver.findElement(By.linkText("Find owners")).click();
+    driver.findElement(By.name("lastName")).clear();
+    driver.findElement(By.name("lastName")).sendKeys("chedde");
+    driver.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+
+
+  }
+
+  @AfterClass
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
